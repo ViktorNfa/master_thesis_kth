c39246b191d705b15b3360b99992e8778fe99594
viktornfa <viktornfa@gmail.com>
2021-09-29 14:32:12 +0200

    Finish formation controller for first task

--- first_task_formation_cbf/src/formation_keeping.py
+++ first_task_formation_cbf/src/formation_keeping.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 #=====================================
-#     Follower controller for the        
-#          SML Nexus robot
+#       Formation keeping with
+#     communication maintenance
+#       or obstacle avoidance 
+#      for the SML Nexus robot
 #               ----
-#   Listen to both the robot pose 
-# and another tracked object from
-#    the mocap system and output a
-#   velocity command to drive the
-#    robot to the follow the object
+#     Listen to both the robot pose 
+#   and the other neighbouring robots 
+#  from the mocap system and output a
+#    velocity command to drive the
+#   robot to keep the formation and
+#  keep the communication bw robots or
+#   avoid colliding with other robots
 #=====================================
 import sys
 import rospy
@@ -17,7 +21,7 @@ import tf2_ros
 import tf2_geometry_msgs
 from tf.transformations import euler_from_quaternion
 
-class FollowerController():
+class CBFFormationController():
     #=====================================
     #         Class constructor
     #  Initializes node and subscribers
@@ -27,44 +31,62 @@ class FollowerController():
         # Initialisation 
         #----------------
         #Initialize node
-        rospy.init_node('follower_controller')
+        rospy.init_node('cbf_formation_controller')
 
-        #Get tracked object name from parameters
+        #Get the robot's name from parameters
         robot_name = rospy.get_param('~robot_name')
 
-        #Get tracked object name from parameters
-        tracked_object = rospy.get_param('~tracked_object')
+        #Get robot number from the name (works for numbers>9)
+        robot_number = int("".join(map(str, [int(i) for i in list(robot_name) if i.isdigit()])))
 
-        #Get offset from tracked object
-        tracking_offset = rospy.get_param('~tracking_offset')
+        #Get tracked neighbour names from parameters
+        neighbours = rospy.get_param('~neighbours')
+        Ni = [i for i in neighbours if i]
+
+        #Get number of neighbours and total number of agents
+        number_neighbours = rospy.get_param('~number_neighbours')
+        num_neighbours = number_neighbours[0]
+        num_agents = number_neighbours[1]
+
+        #Get the ideal positions of the formation
+        formation_positions = rospy.get_param('~formation_positions')
+
+        #Get parameter representing communication maintenance or collision avoidance
+        cbf_direction = rospy.get_param('~cbf_direction')
+
+        #Maximum safe distance for CBF
+        safe_distance = rospy.get_param('~safe_distance')
 
         #Controller gains (for x, y, heading)
-        gains = (2, 2, 2)
+        gains = (1, 1, 1)
 
         #Init robot pose
         self.robot_pose = geometry_msgs.msg.PoseStamped()
 
-        #Init tracked object pose
-        self.tracked_object_pose = geometry_msgs.msg.PoseStamped()
+        #Init neighbouring pose
+        self.neighbour_pose = geometry_msgs.msg.PoseStamped()
 
         #Init last received pose time
         self.last_received_robot_pose = rospy.Time()
 
         #Init last received pose time
-        self.last_received_tracked_pose = rospy.Time()
+        self.last_received_neighbour = rospy.Time()
 
         #Timeout in seconds
         timeout = 0.5
 
         #Booleans to check is positions have been received
         init_pose = False
-        init_track = False
+        init_neighbours = []
+        init_neighbour = False
 
         #Setup robot pose subscriber
         rospy.Subscriber("/qualisys/"+robot_name+"/pose", geometry_msgs.msg.PoseStamped, self.robot_pose_callback)
 
-        #Setup tracked object pose subscriber
-        rospy.Subscriber("/qualisys/"+tracked_object+"/pose", geometry_msgs.msg.PoseStamped, self.tracked_object_pose_callback)
+        #Setup neighbour pose subscriber
+        for i in range(num_neighbours):
+            neighbour_number = int("".join(map(str, [int(j) for j in list(Ni[i]) if j.isdigit()])))
+            rospy.Subscriber("/qualisys/"+Ni[i]+"/pose", geometry_msgs.msg.PoseStamped, self.neighbour_pose_callback, neighbour_number)
 
         #Setup velocity command publisher
         vel_pub = rospy.Publisher("cmd_vel", geometry_msgs.msg.Twist, queue_size=100)
@@ -87,25 +109,31 @@ class FollowerController():
         loop_frequency = 50
         r = rospy.Rate(loop_frequency)
 
-        rospy.loginfo("Follower controller Initialized for "+robot_name+
-                      " tracking "+tracked_object+
-                      " with offset x:"+str(tracking_offset[0])+
-                      ", y:"+str(tracking_offset[1])+
-                      ", heading:"+str(tracking_offset[2])
+        rospy.loginfo("CBF-Formation controller Initialized for "+robot_name+
+                      " with "+str(num_neighbours)+ " neighbours "+str(Ni)+
+                      " with ideal positions "+str(formation_positions)
                       )
         
         while not rospy.is_shutdown():
-            #Run controller if robot position and tracked object position have been received
+            #Run controller if robot position and neighbours position have been received
             now  = rospy.Time.now()
             init_pose = (now.to_sec() < self.last_received_robot_pose.to_sec() + timeout)
-            init_track = (now.to_sec() < self.last_received_tracked_pose.to_sec() + timeout)
+            for t in range(num_neighbours):
+                init_neighbours[t] = (now.to_sec() < self.last_received_neighbour[t].to_sec() + timeout)
+            init_neighbour = max(init_neighbours)
 
-            if init_pose and init_track:
+            if init_pose and init_neighbour:
                 #-----------------------------------
                 # Compute error on x and y position
                 #-----------------------------------
-                error_x = (self.tracked_object_pose.pose.position.x + tracking_offset[0]) - self.robot_pose.pose.position.x 
-                error_y = (self.tracked_object_pose.pose.position.y + tracking_offset[1]) - self.robot_pose.pose.position.y
+                error_x = 0
+                error_y = 0
+                for i in range(num_neighbours):
+                    neighbour_number = int("".join(map(str, [int(j) for j in list(Ni[i]) if j.isdigit()])))
+                    error_x += self.neighbour_pose[neighbour_number-1].pose.position.x - self.robot_pose.pose.position.x + \
+                        (formation_positions[robot_number-1][0] - formation_positions[neighbour_number-1][0])
+                    error_y += self.neighbour_pose[neighbour_number-1].pose.position.y - self.robot_pose.pose.position.y  + \
+                        (formation_positions[robot_number-1][1] - formation_positions[neighbour_number-1][1])
 
                 #-----------------------
                 # Compute heading error
@@ -116,13 +144,15 @@ class FollowerController():
                                                             self.robot_pose.pose.orientation.z,
                                                             self.robot_pose.pose.orientation.w])
 
-                #Get euler angle from robot pose quaternion
-                (track_roll, track_pitch, track_yaw) = euler_from_quaternion([self.tracked_object_pose.pose.orientation.x,
-                                                                              self.tracked_object_pose.pose.orientation.y,
-                                                                              self.tracked_object_pose.pose.orientation.z,
-                                                                              self.tracked_object_pose.pose.orientation.w])
-
-                error_heading = (track_yaw + tracking_offset[2]) - yaw
+                #Get euler angle from neighbours pose quaternion
+                error_heading = 0
+                for i in range(num_neighbours):
+                    neighbour_number = int("".join(map(str, [int(j) for j in list(Ni[i]) if j.isdigit()])))
+                    (neighbour_roll, neighbour_pitch, neighbour_yaw) = euler_from_quaternion([self.neighbour_pose[neighbour_number-1].pose.orientation.x,
+                                                                              self.neighbour_pose[neighbour_number-1].pose.orientation.y,
+                                                                              self.neighbour_pose[neighbour_number-1].pose.orientation.z,
+                                                                              self.neighbour_pose[neighbour_number-1].pose.orientation.w])
+                    error_heading += neighbour_yaw - yaw
 
                 #----------------
                 # Compute output
@@ -168,14 +198,14 @@ class FollowerController():
 
     #=====================================
     #          Callback function 
-    #  for tracked object pose feedback
+    #    for neighbours pose feedback
     #=====================================
-    def tracked_object_pose_callback(self, pose_stamped_msg):
-        #Save robot pose as class variable
-        self.tracked_object_pose = pose_stamped_msg
+    def neighbour_pose_callback(self, pose_stamped_msg, neighbour_number):
+        #Save neigbours pose as class variable
+        self.neighbour_pose[neighbour_number-1] = pose_stamped_msg
 
-        #Save when last pose was received
-        self.last_received_tracked_pose = rospy.Time.now()
+        #Save when last neighbours pose was received
+        self.last_received_neighbour_pose[neighbour_number-1] = rospy.Time.now()
 
 
 #=====================================
@@ -206,7 +236,7 @@ def transform_twist(twist= geometry_msgs.msg.Twist, transform_stamped = geometry
 #               Main
 #=====================================
 if __name__ == "__main__":
-    follower_controller = FollowerController()
+    cbf_follower_controller = CBFFormationController()
     try:
         rospy.spin()
     except ValueError as e:
